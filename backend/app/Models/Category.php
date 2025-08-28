4befd5511e02189a3ad2a58e9bb9015b1f68849e
Natasa Matovic <nacamatovic47@gmail.com>
2025-08-28 22:53:31 +0200

    dodati modeli

---
+++ backend/app/Models/Category.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name',
+        'tag',
+        'user_id'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function task(){
+        return $this->hasMany(Task::class);
+    }
+}
