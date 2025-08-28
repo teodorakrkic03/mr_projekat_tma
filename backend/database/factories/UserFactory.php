8dc20b93a91ac94b68c925c06748f4340badd7ab
Natasa Matovic <nacamatovic47@gmail.com>
2025-08-28 22:59:43 +0200

    dodati factories

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -24,11 +24,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'username' => fake()->unique()->userName(),
+            'email' => fake()->unique()->email(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'role' => 'user',
+            'is_verified' => fake()->boolean(70)
         ];
     }
 
@@ -37,8 +39,8 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
+        return $this->state(fn () => [
+            'is_verified' => false,
         ]);
     }
 }
